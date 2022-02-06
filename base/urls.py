1f940b8567e242d388631504c95ab23a23f780ed
Christian <abduladeneye@gmail.com>
2022-02-06 21:58:05 +0100

    refactored url for debug toolbar

--- base/urls.py
+++ base/urls.py
@@ -2,9 +2,16 @@ import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
 
+from base import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # django debug toolbar
-    path('__debug__/', include(debug_toolbar.urls)),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
